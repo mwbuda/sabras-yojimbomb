b5e197683d3c34da3654b1081b7147d2a0b7fb09
mwbuda <mbuda@sabrassoft.com>
2015-10-22 23:34:14 -0400

    changes base metric, period metric, event metric, & metric criteria domain modeling

--- yojimbomb-rb/lib/yojimbomb/schema/base.rb
+++ yojimbomb-rb/lib/yojimbomb/schema/base.rb
@@ -4,6 +4,12 @@ require 'yojimbomb/schema/datetime'
 
 module Yojimbomb
 
+	# a nonsense error is raised/thrown whenver a nonsensical situation is created;
+	#	eg. declaring a stop time before a start time.
+	class NonsenseError < StandardError
+		
+	end
+	
 	class Metric
 		attr_reader :metricType, :metricClass, :count, :occurence, :id
 		
@@ -45,8 +51,92 @@ module Yojimbomb
 			Yojimbomb::DateTime.dayOfWeek(@occurence, zone)
 		end
 		
+		def timeOfDay(zone = nil)
+			Yojimbomb::DateTime.timeOfDay(@occurence,zone)
+		end
+		
+		def match(criteria)
+			[
+				matchOccurence(criteria),
+				matchTimeOfDay(criteria),
+				matchDayOfWeek(criteria),
+				matchPrimaryTags(criteria),
+				matchMinorTags(criteria)
+			].reduce(:&)
+		end
+		
+		def matchOccurence(criteria)
+			(@occurence >= criteria.start) and (@occurence <= criteria.stop)
+		end
+		
+		def matchTimeOfDay(criteria)
+			return true if criteria.todStart.nil?
+			tod = self.timeOfDay(criteria.timezone)
+			(tod >= criteria.todStart) and (tod <= criteria.todStop)
+		end
+		
+		def matchDayOfWeek(criteria)
+			return true if criteria.dow.nil?
+			my_wd = Yojimbomb::DateTime.numericWeekDay(self.dayOfWeek(criteria.timezone))
+			c_wd = Yojimbomb::DateTime.numericWeekDay(criteria.dow) 
+			my_wd == c_wd
+		end
+		
+		def matchPrimaryTags(criteria)
+			return true if criteria.primaryTags.empty?
+			criteria.primaryTags.each {|cpt| return false unless @primaryTags.include?(cpt) }
+			true
+		end
+		
+		def matchMinorTags(criteria)
+			return true if criteria.minorTags.empty?
+			criteria.minorTags.each {|cmt| return false unless @minorTags.include?(cmt) }
+			true
+		end
+		
 	end
 	
+	class Criteria
+		attr_reader :start, :stop
+		attr_reader :todStart, :todStop, :dow, :timezone
+		attr_reader :primaryTags, :minorTags
+		
+		TodDefaultIncrement = 30
+		DefaultSundry = {
+			:todIncrement => 1,
+			:timezone => 0
+		}
+		
+		def initialize(start,stop, sundry = {})
+			@start = Yojimbomb::DateTime.parseTime(start)
+			@stop = Yojimbomb::DateTime.parseTime(stop)
+			
+			xsundry = DefaultSundry.dup
+			unless sundry.nil?
+				csundry = sundry.select {|k,v| !v.nil?}
+				xsundry.merge!(csundry)
+			end
+			
+			@todStart = xsundry[:todStart]
+			@todStart = xsundry[:tod] if todStart.nil?
+			
+			unless todStart.nil?
+				@todStop = xsundry[:todStop]
+				@todStop = todStart + xsundry[:todIncrement] if @todStop.nil?
+			end
+				
+			@dow = xsundry[:dow]
+				
+			@timezone = xsundry[:timezone] 
+				
+			@primaryTags = []
+			@primaryTags += xsundry[:primaryTags] unless xsundry[:primaryTags].nil?
+				
+			@minorTags = []
+			@minorTags += xsundry[:minorTags] unless xsundry[:minorTags].nil?
+		end
+	
+	end
 	
 end
 
--- yojimbomb-rb/lib/yojimbomb/schema/period.rb
+++ yojimbomb-rb/lib/yojimbomb/schema/period.rb
@@ -9,20 +9,85 @@ module Yojimbomb
 		def initialize(type, start, stop, duration, sundry = {})
 			super.initialize(type, :period, start, sundry)
 			@stop = Yojimbomb::DateTime.parseTime(stop)
-			@duration = (duration == :whole) ? (self.stop - self.start) : duration
 			
+			if (duration == :whole)
+				@duration = (self.stop - self.start)
+				@todStart = Yojimbomb::DateTime.timeOfDay(self.start)
+				@todStop = Yojimbomb::DateTime.timeOfDay(self.stop)
+			else
+				@duration = duration
+			
+				if sundry.keys.include?(:tod) or sundry.keys.include?(:todStart)
+					@todStart = sundry[:todStart]
+					@todStart = sundry[:tod] if @todStart.nil?
+					@todStop = sundry[:todStop]	
+				else
+					@todStart = Yojimbomb::DateTime.timeOfDay(self.start)
+				end
+				
+				if @todStop.nil?
+					num_days = Yojimbomb::DateTime.daysBetween(self.start,self.stop) + 1
+					mins_per_day = @duration / num_days
+					elapsed_change = ((mins_per_day * 10) / 6).to_f / 100.0
+					@todStop = @todStart + elapsed_change
+				end
+			end
 		end
 		
 		alias :start :occurence
 		alias :startDayOfWeek :dayOfWeek
 		
-		def endDayOfWeek
-			Yojimbomb::DateTime.dayOfWeek(@stop)
+		def timeOfDay(zone = nil)
+			tod_time = Yojimbomb::DateTime.changeToTimeOfDay(self.start,@todStart)
+			Yojimbomb::DateTime.timeOfDay(tod_time,criteria.zone)
 		end
 		
-		def alter(duration, stop)
-			@duration = duration
-			@stop = Yojimbomb::DateTime.parseTime(stop)
+		alias :startTimeOfDay :timeOfDay
+				
+		def stopTimeOfDay(zone = nil)
+			tod_time = Yojimbomb::DateTime.changeToTimeOfDay(self.start,@todStop)
+			Yojimbomb::DateTime.timeOfDay(tod_time,criteria.zone)
+		end
+		
+		def endDayOfWeek(zone = nil)
+			Yojimbomb::DateTime.dayOfWeek(@stop, zone)
+		end
+		
+		def matchOccurence(criteria)
+			super(criteria) and (self.stop <= criteria.stop)
+		end
+		
+		def matchTimeOfDay(criteria) 
+			return true if critera.todStart.nil?
+			
+			stod = self.startTimeOfDay(criteria.zone)
+			etod = self.stopTimeOfDay(critera.zone)
+			
+			if (stod >= criteria.todStart) and (stod <= criteria.todStop)
+				true
+			elsif (etod >= criteria.todStart) and (etod <= criteria.todStop)
+				true
+			else
+				false
+			end
+		end
+		
+		def matchDayOfWeek(criteria)
+			return true if criteria.dow.nil?
+			
+			my_swd = Yojimbomb::DateTime.numericWeekDay( self.startDayOfWeek(criteria.zone) )
+			my_ewd = Yojimbomb::DateTime.numericWeekDay( self.endDayOfWeek(criteria.zone) )
+			
+			range = if my_swd < my_ewd
+				(my_swd..my_ewd).to_a
+			else
+				sow = Yojimbomb::DateTime.numericWeekDay(Yojimbomb::DateTime::DaysOfWeek[0])
+				eow = Yojimbomb::DateTime.numericWeekDay(Yojimbomb::DateTime::DaysOfWeek[-1])
+				(my_swd..eow).to_a + (sow..my_ewd).to_a					  
+			end
+			
+			c_wd = Yojimbomb::DateTime.numericWeekDay(criteria.dow)
+			range.include?(c_wd)
 		end
 		
 	end
