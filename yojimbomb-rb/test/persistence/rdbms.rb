74057942196fb7eece872dbfd8f60123d3b6f29e
mwbuda <mbuda@sabrassoft.com>
2015-11-12 21:09:35 -0500

    cleanup to RDBMS test

--- yojimbomb-rb/test/persistence/rdbms.rb
+++ yojimbomb-rb/test/persistence/rdbms.rb
@@ -1,8 +1,16 @@
 
-require 'base'
-require 'yojimbomb/persistence/rdbms/sequel'
+require './base'
+require 'yojimbomb/persistence/rdbms'
 
-$DBConnect = $*[0]
+#$DBConnect = $*[0]
+$DBConnect = {
+	:adapter => 'mysql2',
+	:host => 'localhost',
+	:database => 'yojimbomb_test',
+	:user => 'ybtest',
+	:password => 'password',	
+}
 
-metricsKeeper = Yojimbomb::RDBMS::SequelMetricsKeeper.new($DBConnect)
+metricsKeeper = Yojimbomb::RdbmsMetricsKeeper.connect($DBConnect)
+metricsKeeper.logger = Logger.new(STDOUT)
 Yojimbomb::Test.testPersistence(metricsKeeper)
