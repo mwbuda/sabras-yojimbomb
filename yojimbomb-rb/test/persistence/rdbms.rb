d614327ea733ec5304d8dc76f9e1f95290eab375
mwbuda <mbuda@sabrassoft.com>
2015-11-13 04:01:02 -0500

    RDBMS test: + monkey patched in debug output to see SQL

--- yojimbomb-rb/test/persistence/rdbms.rb
+++ yojimbomb-rb/test/persistence/rdbms.rb
@@ -1,6 +1,19 @@
 
 require './base'
 require 'yojimbomb/persistence/rdbms'
+require 'mysql2'
+
+class Mysql2::Client
+	
+	def query(sql, options = {})
+		puts "!!!SQL: #{sql}"
+		@query_options.each {|opt,v| puts "!!!QOPT #{opt} = v"} unless @query_options.nil?
+		options.each {|opt,v| puts "!!!XOPT #{opt} = #{v}"} 
+		_query(sql, @query_options.merge(options))
+	end
+	
+end
+
 
 #$DBConnect = $*[0]
 $DBConnect = {
